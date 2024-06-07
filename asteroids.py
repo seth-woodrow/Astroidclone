3e676e87b81bfa37280b3d0cf727d3e63f9ebba3
Seth Woodrow <seth.woodrow0@gmail.com>
2024-06-06 20:39:14 -0500

    set up window and pen

--- asteroids.py
+++ asteroids.py
@@ -1,3 +1,14 @@
 import turtle
 import math
 import random
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("AstroidClone")
+wn.setup(800, 600)
+wn.tracer(0)
+
+pen = turtle.Turtle()
+pen.penup()
+pen.speed(0)
+
