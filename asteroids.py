7b0cc5c064cbc60b353448cf477ac00a5ed89c27
Seth Woodrow <seth.woodrow0@gmail.com>
2024-06-09 13:02:11 -0500

    created palyer and asteroid classes

--- asteroids.py
+++ asteroids.py
@@ -61,4 +61,36 @@ class Sprite():
         self.x = x
         self.y = y
 
-        
+
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.shape = "triangle"
+        self.lives = 3
+        self.score = 0
+
+        def rotate_left(self):
+            self.heading += 30
+
+        def rotate_right(self):
+            self.heading -= 30
+
+        def accelerate(self):
+            ax = math.cos(math.radians(self.heading))
+            ay = math.sin(math.radians(self.heading))
+            self.dx += ax * 0.1
+            self.dy += ay * 0.1
+
+        def render(self, pen):
+            if self.active:
+                pen.goto(self.x, self.y)
+                pen.setheading(self.heading)
+                pen.shape(self.shape)
+                pen.shapesize(self.size/2.0, self.size, 0)
+                pen.color(self.color)
+                pen.stamp()
+
+class Asteroid(Sprite):
+    def __init__(self):
+        Sprite().__init__(self)
+        self.shape = "circle"
